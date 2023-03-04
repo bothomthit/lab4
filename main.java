f23899542deaee86ac5430dc7f8cc7d09f787757
bothomthit <125108405+bothomthit@users.noreply.github.com>
2023-03-04 23:00:18 +0700

    Add files via upload

--- main.java
+++ main.java
@@ -2,15 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Cricle;
+package HinhHoc;
 
 /**
  *
  * @author nhu quynh
  */
 public class main {
-    public static void main(String[] args) {
- Cylinder quynh = new Cylinder("rainbow",15,5  );
- System.out.print(quynh.getArea());
-}          
+   public static void main(String[] args) {
+        HinhTru ht =new HinhTru();
+        ht.nhapchieuCao();
+        ht.nhapBK();
+       
+        System.out.println(ht.getTheTich());
+        hinhChuNhat hcn = new hinhChuNhat();
+        hcn.nhapChieuDai();
+        hcn.nhapChieuRong();
+        System.out.println("dien tich hinh chu nhat la" +hcn.getDienTich());
+        System.out.println("chu vi hinh chu nhat la" +hcn.getChuVi());
+        HinhTron htr = new HinhTron();
+        htr.nhapBK();
+        System.out.println("dien tich hinh tron la" +htr.getDienTich());
+        
+    } 
+   
 }
